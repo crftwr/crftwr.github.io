0732a4f95ad2b035485017ed0f4e5f76579bfe8f
crftwr <craftware@gmail.com>
2013-08-11 18:14:24 +0900

    LREdit用のindex.html

--- tool/build.py
+++ tool/build.py
@@ -9,4 +9,5 @@ PYTHON_DIR = "c:/python33"
 PYTHON = PYTHON_DIR + "/python.exe"
 
 subprocess.call( [ PYTHON, "tool/rst2html_pygments.py", "--stylesheet=tool/rst2html_pygments.css", "index.txt", "index.html" ] )
+subprocess.call( [ PYTHON, "tool/rst2html_pygments.py", "--stylesheet=tool/rst2html_pygments.css", "lredit/index.txt", "lredit/index.html" ] )
 
